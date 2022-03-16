668ecac6489308c2049f5e5c233e4bb6643266c4
CHO1116 <dudwnscho@naver.com>
2022-03-16 16:35:14 +0900

    rename variable name

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 14:10:36 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/11 22:06:47 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/16 15:03:05 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	index;
+	size_t	i;
 
-	index = 0;
-	while ((s1[index] != '\0' || s2[index] != '\0') && index < n)
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (s1[index] == s2[index])
-			index++;
+		if (s1[i] == s2[i])
+			i++;
 		else
-			return ((unsigned char) s1[index] - (unsigned char) s2[index]);
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 	}
 	return (0);
 }
