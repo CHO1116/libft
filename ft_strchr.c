40ded160af089cd86519b2e8e183cebcd4fa151a
CHO1116 <dudwnscho@naver.com>
2022-03-22 13:49:05 +0900

    fix return value

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 14:09:30 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/21 19:12:25 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/22 13:23:56 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ char	*ft_strchr(const char *s, int c)
 	while (len--)
 	{
 		if (*s == (unsigned char)c)
-			return (s);
+			return ((char *)s);
 		s++;
 	}
-	return (NULL);
+	return (0);
 }
