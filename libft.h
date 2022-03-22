ace62cf89bd8b892f08cc73b255010b09030f8ef
CHO1116 <dudwnscho@naver.com>
2022-03-22 16:44:11 +0900

    delete all annotation

--- libft.h
+++ libft.h
@@ -6,15 +6,13 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 17:45:09 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/21 15:23:09 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/22 16:42:28 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-//for using 'size_t' type
 # include <unistd.h>
-//man function in term
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
@@ -38,7 +36,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-//create new function
 char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
@@ -50,13 +47,11 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-//new define about linked list with struct
 typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }	t_list;
-//bonus function related to linked list
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
