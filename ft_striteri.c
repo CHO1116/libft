e6fadf19ee570d140face65ea8afbbd9ebd77137
CHO1116 <dudwnscho@naver.com>
2022-03-22 19:19:08 +0900

    each factors of function can have any function

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,22 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 14:09:46 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/21 17:50:34 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/22 19:12:31 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
+	unsigned int	num;
 
+	if (!s)
+		return ;
+	num = 0;
+	while (s[num] != '\0')
+	{
+		f(num, &s[num]);
+		num++;
+	}
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 14:10:27 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/21 17:50:43 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/22 19:12:02 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,22 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	unsigned int	num;
+	size_t			len;
+	char			*src;
 
+	if (!s || !f)
+		return (0);
+	num = 0;
+	len = ft_strlen(s);
+	src = (char *)malloc(sizeof(char) * (len + 1));
+	if (!src)
+		return (0);
+	while (s[num] != '\0')
+	{
+		src[num] = f(num, s[num]);
+		num++;
+	}
+	src[num] = '\0';
+	return (src);
 }
