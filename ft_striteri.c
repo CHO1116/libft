855dd1d9ec6621ed8fea0be4da76b89adbd344e2
CHO1116 <dudwnscho@naver.com>
2022-03-23 19:01:15 +0900

    Check NULL Guard

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 14:09:46 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/22 19:12:31 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/23 17:03:33 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	num;
 
-	if (!s)
+	if (!s || !f)
 		return ;
 	num = 0;
 	while (s[num] != '\0')
