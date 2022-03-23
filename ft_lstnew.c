2966d8ea707b22d24ce93beb8f713fb574f8b46b
CHO1116 <dudwnscho@naver.com>
2022-03-23 19:13:28 +0900

    change expression

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 00:50:42 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/23 14:05:27 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/23 19:04:00 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstnew(void *content)
 
 	new = (t_list *)malloc(sizeof(t_list));
 	if (!new)
-		return (0);
+		return (NULL);
 	new->content = content;
 	new->next = NULL;
 	return (new);
