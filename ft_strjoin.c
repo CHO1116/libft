269b29e6c5208228358af05dd4c0d087b839dbc9
CHO1116 <dudwnscho@naver.com>
2022-03-23 14:58:29 +0900

    join s1 and s2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 14:09:52 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/21 17:50:37 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/23 14:37:48 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*new_str;
 
+	if (!s1 || !s2)
+		return (0);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!new_str)
+		return (0);
+	ft_memcpy(new_str, s1, s1_len);
+	ft_memcpy(new_str + s1_len, s2, s2_len);
+	new_str[s1_len + s2_len] = '\0';
+	return (new_str);
 }
