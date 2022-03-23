bc99c11611e84ca723f5f4fab354bc3002a93157
CHO1116 <dudwnscho@naver.com>
2022-03-23 19:00:20 +0900

    recheck

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,13 +6,46 @@
 /*   By: youngjch <youngjch@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 00:50:39 by youngjch          #+#    #+#             */
-/*   Updated: 2022/03/23 01:06:12 by youngjch         ###   ########.fr       */
+/*   Updated: 2022/03/23 18:41:41 by youngjch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+
+static void	free_all(t_list *lst)
+{
+	t_list	*tmp;
+
+	while (lst)
+	{
+		tmp = lst->next;
+		free(lst);
+		lst = tmp;
+	}
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*new_list;
+	t_list	*new_node;
+
+	if (!lst || !f)
+		return (NULL);
+	new_list = NULL;
+	while (lst != NULL)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		ft_lstadd_back(&new_list, new_node);
+		if (!new_node)
+		{
+			if (!del)
+				free_all(new_list);
+			else
+				ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		lst = lst->next;
+	}
+	return (new_list);
 }
